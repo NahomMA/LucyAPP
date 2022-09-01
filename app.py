0e1880bbb26b7dfa47c442e1906c872f2d77c03c
NahomMA <nahommengistu13@gmail.com>
2022-09-01 16:27:03 +0200

    Lucy app final

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -28,4 +29,67 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+# data_load_state.text('Loading data...done!')
+
+
+# '''
+
+# You don’t want to reload the data each time 
+# the app is updated – luckily Streamlit allows 
+# you to cache the data.
+
+# Effortless caching
+
+# Try adding @st.cache before the load_data declaration:
+
+
+# '''
+
+data_load_state.text("Done! (using st.cache)")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+#Plot data on a map
+# Add a subheader for the section
+
+# st.subheader('Map of all pickups')
+# st.map(data)
+
+
+# st.subheader('Map of all pickups')
+# st.map(data)
+
+
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+
+# Filter results with a slider
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
